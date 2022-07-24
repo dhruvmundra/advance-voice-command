1f7eeab57d6a756fbb1ac0331666d14b484960d1
dhruvmundra <81858843+dhruvmundra@users.noreply.github.com>
2022-07-24 16:47:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,12 @@ x = 0;
 y = 0;
 
 draw_apple = "";
-
+apple=""
+speak_data=""
+to_number=0
+function preload(){
+  apple=loadImage("apple.png")
+}
 var SpeechRecognition = window.webkitSpeechRecognition;
   
 var recognition = new SpeechRecognition();
@@ -20,17 +25,29 @@ recognition.onresult = function(event) {
  content = event.results[0][0].transcript;
 
     document.getElementById("status").innerHTML = "The speech has been recognized: " + content; 
-
+to_number=Number(content);
+draw_apple="set"
 }
 
 function setup() {
- 
+ screen_width=window.innerWidth;
+ screen_height=window.innerHeight;
+
+ canvas=createCanvas(screen_width,screen_height);
+ canvas.position(0,150)
 }
 
 function draw() {
   if(draw_apple == "set")
   {
+    for(var i=1;i<=to_number;i++){
+      x=Math.floor(Math.random()*700);
+      y=Math.floor(Math.random()*400);
+      image(apple,x,y,50,50);
+    }
     document.getElementById("status").innerHTML = to_number + " Apples drawn";
+    speak_data=to_number+"apple drawn";
+    speak();
     draw_apple = "";
   }
 }
